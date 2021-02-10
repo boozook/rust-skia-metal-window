6be01f67e2ffcc4a629ceb8e0cedb48f976e5549
Alexander Koz <a@koz.email>
2021-02-10 22:51:54 +0300

    try fix render

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,36 @@
+use std::mem;
 use metal::*;
-use cocoa::{appkit::NSView, base::id as cocoa_id};
 use foreign_types::ForeignType;
+use foreign_types::ForeignTypeRef;
 use objc::{rc::autoreleasepool, runtime::YES};
+use cocoa::{appkit::NSView, base::id as cocoa_id};
 use skia::ColorSpace;
 use skia::ColorType;
 use skia::Surface;
 use skia::colors::WHITE;
+use skia::gpu::DirectContext;
+use skia::gpu::BackendRenderTarget;
 use skia::gpu::SurfaceOrigin;
+use skia::gpu::mtl::TextureInfo;
 use winit_input_helper::WinitInputHelper;
 use winit::platform::macos::WindowExtMacOS;
 use winit::event::{Event, WindowEvent, VirtualKeyCode};
-use winit::event_loop::ControlFlow;
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::window::Window;
 
 
 mod renderer;
 
 
 fn main() {
-	let events_loop = winit::event_loop::EventLoop::new();
+	let events_loop = EventLoop::new();
 	let mut input = WinitInputHelper::new();
 
 	let size = winit::dpi::LogicalSize::new(1042, 1042);
 
 	let window = winit::window::WindowBuilder::new().with_inner_size(size)
 	                                                .with_title("Skia with Metal backend".to_string())
+	                                                // .with_resizable(false)
 	                                                .build(&events_loop)
 	                                                .unwrap();
 
@@ -32,14 +39,18 @@ fn main() {
 	let layer = MetalLayer::new();
 	layer.set_device(&device);
 	layer.set_pixel_format(MTLPixelFormat::BGRA8Unorm);
-	layer.display_sync_enabled();
+	// layer.set_presents_with_transaction(false);
+	// layer.display_sync_enabled();
+
 	layer.set_framebuffer_only(false);
 	layer.set_opaque(true);
+
+	layer.set_maximum_drawable_count(3);
 
 	unsafe {
 		let view = window.ns_view() as cocoa_id;
 		view.setWantsLayer(YES);
-		view.setLayer(layer.as_ptr() as *mut _ as cocoa_id);
+		view.setLayer(mem::transmute(layer.as_ref()));
 	};
 
 	let draw_size = window.inner_size();
@@ -47,42 +58,47 @@ fn main() {
 
 	let command_queue = device.new_command_queue();
 
-	let (mut ctx, (mut surface, drawable)) = {
-		let mut ctx = unsafe {
-			skia::gpu::DirectContext::new_metal(
-			                                    device.as_ptr() as *mut _,
-			                                    command_queue.as_ptr() as *mut _,
-			                                    None,
-			).unwrap()
-		};
 
-		let surface = Surface::from_ca_metal_layer(
-		                                           &mut ctx,
-		                                           layer.as_ptr() as *mut _,
-		                                           SurfaceOrigin::BottomLeft,
-		                                           Some(4),
-		                                           ColorType::BGRA8888,
-		                                           ColorSpace::new_srgb(),
-		                                           None,
-		).expect("Unable to create surface");
-
-		(ctx, surface)
+	let mut ctx = unsafe {
+		DirectContext::new_metal(device.as_ptr() as *mut _, command_queue.as_ptr() as *mut _, None).expect("Unable to create direct context")
 	};
+
+	fn create_surface(ctx: &mut DirectContext, window: &Window, drawable: &MetalDrawableRef) -> Option<Surface> {
+		println!("create_surface start");
+		let draw_size = window.inner_size();
+		let t_info = unsafe { TextureInfo::new(drawable.texture().as_ptr() as *const _) };
+		let target = BackendRenderTarget::new_metal((draw_size.width as i32, draw_size.height as i32), 4, &t_info);
+
+		// TODO: let color_type = layer.pixel_format();
+
+		let surface = Surface::from_backend_render_target(
+		                                                  ctx,
+		                                                  &target,
+		                                                  SurfaceOrigin::BottomLeft,
+		                                                  ColorType::BGRA8888,
+		                                                  ColorSpace::new_srgb(),
+		                                                  None,
+		);
+
+		surface
+	}
 
 
 	let mut frame = 0;
 
 	events_loop.run(move |event, _, control_flow| {
 		           autoreleasepool(|| {
-			           *control_flow = ControlFlow::Poll;
+			           // *control_flow = ControlFlow::Poll;
+			           *control_flow = ControlFlow::Wait;
 
 			           match &event {
 				           Event::WindowEvent { event, .. } => {
 				              match event {
 					              WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
 				                 WindowEvent::Resized(size) => {
+					                 println!("Resized: {:?}", size);
 					                 layer.set_drawable_size(CGSize::new(size.width as f64, size.height as f64));
-					                 // TODO: re-create / resize the surface
+					                 // XXX: re-create / resize the surface
 				                 },
 
 				                 _ => (),
@@ -92,13 +108,32 @@ fn main() {
 				              window.request_redraw();
 			              },
 			              Event::RedrawRequested(_) => {
-				              {
-					              let canvas = surface.canvas();
-					              canvas.clear(WHITE);
-					              renderer::render_frame(frame % 360, 12, 60, canvas);
+				              println!("frame {}", frame);
+
+				              if let Some(drawable) = layer.next_drawable() {
+					              let surface = create_surface(&mut ctx, &window, drawable);
+
+					              if let Some(mut surface) = surface {
+						              // skia render:
+						              let canvas = surface.canvas();
+						              canvas.clear(WHITE);
+						              renderer::render_frame(frame % 360, 12, 60, canvas);
+
+						              surface.canvas().flush();
+						              ctx.flush_and_submit();
+
+						              //   let render_pass_descriptor = RenderPassDescriptor::new();
+						              //   prepare_render_pass_descriptor(&render_pass_descriptor, drawable.texture());
+						              let command_buffer = command_queue.new_command_buffer();
+						              command_buffer.present_drawable(drawable);
+						              command_buffer.commit();
+					              } else {
+						              println!("unable to create surface");
+					              }
+				              } else {
+					              println!("frame skip, no drawable");
 				              }
-				              surface.canvas().flush();
-				              ctx.flush_and_submit();
+				              frame += 1;
 			              },
 
 			              _ => {},
